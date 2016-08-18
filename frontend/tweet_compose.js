e51dd9f5ff372eb8a7835be7386766e1f2ea68a0
Munyo Frey <munyofrey@gmail.com>
2016-08-18 15:10:38 -0700

    Refactored pahse 3. Moving on to phase 4

--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -1,19 +1,17 @@
 function TweetCompose($el){
   this.$form = $el;
+  this.label = $el.find('label.text-area');
   this.count = 140;
   this.counter();
-  this.submit();
+  this.$form.submit(this.submit.bind(this));
 }
 
 
-TweetCompose.prototype.submit = function(){
-  this.$form.on('submit', (e)=>{
+TweetCompose.prototype.submit = function(e){
     e.preventDefault();
-    // console.log(this.$form);
+    debugger
     let serializedForm = this.$form.serializeJSON();
     $(':input').prop('disabled', true);
-    // console.log("---------");
-    // console.log(this.$form);
     $.ajax({
       method: 'POST',
       url: '/tweets',
@@ -21,24 +19,20 @@ TweetCompose.prototype.submit = function(){
       data: serializedForm,
       success: (message)=>{
         this.handleSuccess(message);
-        // console.log(message);
       },
       error: (eMessage)=>{
         alert("Incomplete Tweet");
         $(':input').prop('disabled', false);
       }
     });
-  });
 };
 
 TweetCompose.prototype.counter = function(){
   const textarea = this.$form.find('textarea');
-  const label = this.$form.find('label.text-area');
-  label.append($("<strong>").text(`${this.count} charaters remaining`));
+  this.label.append($("<strong>").text(`${this.count} charaters remaining`));
   textarea.on('input',(e)=>{
-    console.log(textarea.val());
-    label.children().last().remove();
-    label.append($("<strong>").text(`${this.count - textarea.val().length} charaters remaining`));
+    this.label.children().last().remove();
+    this.label.append($("<strong>").text(`${this.count - textarea.val().length} charaters remaining`));
   });
 };
 
@@ -48,11 +42,15 @@ TweetCompose.prototype.clearInput = function(){
 };
 
 TweetCompose.prototype.handleSuccess = function(message){
+
+  this.label.children().last().remove();
+  this.label.append($("<strong>").text(`${this.count} charaters remaining`));
+
+
   let ul = $('ul#feed');
   let string = JSON.stringify(message);
   const newTweet = $('<li>').text(string);
   ul.prepend(newTweet);
-  console.log(ul);
   this.clearInput();
 };
 
