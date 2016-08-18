9451e85cb3012d492ea968a52ed8505f2aa0dd28
Munyo Frey <munyofrey@gmail.com>
2016-08-18 15:40:26 -0700

    Finished phase 4. Added multiple selections for mentions

--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -3,13 +3,13 @@ function TweetCompose($el){
   this.label = $el.find('label.text-area');
   this.count = 140;
   this.counter();
+  this.$form.find('a.add-mentioned-user').click(this.addMentionedUser.bind(this));
   this.$form.submit(this.submit.bind(this));
 }
 
 
 TweetCompose.prototype.submit = function(e){
     e.preventDefault();
-    debugger
     let serializedForm = this.$form.serializeJSON();
     $(':input').prop('disabled', true);
     $.ajax({
@@ -39,19 +39,30 @@ TweetCompose.prototype.counter = function(){
 TweetCompose.prototype.clearInput = function(){
   this.$form.trigger('reset');
   $(':input').prop('disabled', false);
+  $('.mentioned-users').children().remove();
 };
 
 TweetCompose.prototype.handleSuccess = function(message){
-
   this.label.children().last().remove();
   this.label.append($("<strong>").text(`${this.count} charaters remaining`));
-
-
   let ul = $('ul#feed');
   let string = JSON.stringify(message);
   const newTweet = $('<li>').text(string);
   ul.prepend(newTweet);
   this.clearInput();
 };
+
+TweetCompose.prototype.addMentionedUser = function(e){
+  let renderHTML = $(".list-users").html();
+  $('.mentioned-users').append(renderHTML);
+  this.$form.find('a.remove-mentioned-user').click(this.removeMentionedUser.bind(this));
+};
+
+TweetCompose.prototype.removeMentionedUser = function(e){
+  let $currentTarget = $(e.currentTarget);
+  $currentTarget.parent().remove();
+
+};
+
 
 module.exports = TweetCompose;
